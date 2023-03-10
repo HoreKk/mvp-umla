d69d5e8a99c28ad9efad49cca414fea01d5c07bd
horek <azenoxe@gmail.com>
2023-03-11 00:10:40 +0100

    chore: init

---
+++ tailwind.config.cjs
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	theme: {
+		fontFamily:{
+			'sans': ['Kanit'],
+		},
+		extend: {
+			colors: {
+				'umla-dark-blue': '#253c5d',
+				'umla-blue': '#2a85b8',
+				'umla-light-blue': '#2da2bb',
+				'umla-green': '#2d635f',
+				'umla-sand': '#c1b276',
+				'umla-red': '#b9191c',
+				'umla-yellow': '#fbf951',
+			}
+		},
+	},
+	plugins: [],
+}
